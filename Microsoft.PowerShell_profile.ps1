352df472daee5d1c81aa833ab61258cfae5395bc
botus99 <botus99@gmail.com>
2023-09-25 21:46:22 +0000

    Add files via upload

---
+++ Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,79 @@
+### The default location for this file is...
+### C:\Users\...insert_your_name_here...\Documents\Powershell\
+
+
+#############################
+#         Aliases			#
+#############################
+
+### SCRIPTS ###
+set-alias home "~/Documents/Powershell/home.ps1"
+set-alias sa "~/Documents/Powershell/updatemygoods.ps1"
+
+### SHIMS ###
+set-alias chdman "~/scoop/apps/mame/current/chdman.exe"
+set-alias ffp "~/scoop/apps/firefox-noinstall/current/firefox.exe"
+set-alias firefox "~/scoop/apps/firefox/current/firefox.exe"
+set-alias lolcat "~/scoop/apps/rainbow/current/rainbow.exe"
+set-alias nicotine "~/scoop/apps/nicotine-plus/current/Nicotine+.exe"
+set-alias obsidian "~/scoop/apps/obsidian/current/Obsidian.exe"
+set-alias pmp "~/scoop/apps/plex-desktop/current/Plex.exe"
+set-alias pwsh "~/scoop/apps/powershell-preview/current/pwsh.exe"
+set-alias shutup10 "~/scoop/apps/shutup10/current/OOSU10.exe"
+set-alias vscode "~/scoop/apps/vscodium/current/VSCodium.exe"
+set-alias vscodium "~/scoop/apps/vscodium/current/VSCodium.exe"
+
+### MISC ###
+set-alias info show
+set-alias show info
+set-alias whereami pwd
+
+
+#############################
+#        other crap			#
+#############################
+
+Import-Module 7Zip4Powershell
+Import-Module dockercompletion
+Import-Module NuGet
+Import-Module Pester
+Import-Module Pscx
+Import-Module PowerShellGet
+Import-Module PSEverything
+Import-Module PSFzf
+Import-Module PSFuzzySearch
+Import-Module PSReadLine
+Import-Module PSColors
+Import-Module PSWindowsUpdate
+Import-Module PSWriteColor
+Import-Module psInlineProgress
+Import-Module posh-docker
+Import-Module posh-git
+Import-Module posh-cargo
+Import-Module scoop-completion
+Import-Module Terminal-Icons
+Import-Module Update-AllPSModules
+Import-Module z
+
+set-psreadlineoption -predictionsource history
+set-psreadlineoption -predictionviewstyle listview
+set-psreadlineoption -editmode windows
+set-psreadlineOption -colors @{ inlineprediction = "#8A0303"}
+
+#############################
+#         PROMPT			#
+#############################
+
+oh-my-posh --init --shell pwsh --config "~/scoop/apps/oh-my-posh3/current/themes/me.json" | Invoke-Expression
+Enable-PoshTooltips
+Enable-PoshTransientPrompt
+$env:POSH_GIT_ENABLED = $true
+$env:POSH_DOCKER_ENABLED = $true
+
+#############################
+#	STUFF I ADDED LATER		#
+#############################
+
+Invoke-Expression (&scoop-search --hook)
+winfetch
+cpufetch
